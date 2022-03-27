0da11021d63d795bf095c29bdfa043e049304acb
Aharon Levy <mr.aharon.levy@gmail.com>
2022-03-27 20:32:42 +0300

    aaaa

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ const buttonsColor = ["red" , "blue" , "green" , "yellow" ];
 
 let gamePattern = [];
 let userPattern = [];
+let isRunning = false;
+let firstuse = false;
+let level = 0;
 $(".btn").click(function (event) {
     let usercolor = this.id;
     playsound(usercolor);
@@ -12,7 +15,23 @@ $(".btn").click(function (event) {
     console.log(userPattern);       
 })
 
+$(document).keydown(function (event) {
+    
+   if (firstuse == false) {
+        if (this.key == "a"){
+            isRunning = true;
+            firstuse = true;
+            nextSequence();
+        }
+   } else if (isRunning == false)
+        isRunning = true;
+        level = 0;
+        nextSequence();
+});
+
 function nextSequence() {
+    level++;
+    $("h1").text("Level " + level);
     let rendumNumber = Math.floor(Math.random() * 4);
     let randomChoosenColor = buttonsColor[rendumNumber];
     gamePattern.push(randomChoosenColor);
@@ -26,6 +45,7 @@ function playsound(sound) {
     choosensound = new Audio("src/" + sound + ".ogg")
     choosensound.play()    
 }
+
 
 
 
